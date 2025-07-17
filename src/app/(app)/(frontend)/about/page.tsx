b40ec3b4fa6dcebe273d8e2a4a3698bb0fa65755
Glory135 <adeyemiglr@gmail.com>
2025-07-17 05:42:22 +0100

    Update page.tsx

--- src/app/(app)/(frontend)/about/page.tsx
+++ src/app/(app)/(frontend)/about/page.tsx
@@ -13,19 +13,19 @@ export default function AboutPage() {
           </div>
           <div className="flex-1 flex flex-col gap-5 md:p-10 justify-center text-start">
             <p className="text-xl font-bold">Unleash Your Inner Deity with GreekGod.</p>
-            <p className="text-xl">At GreekGod, we're more than just a clothing brand;
-              we're a movement dedicated to empowering you to
+            <p className="text-xl">At GreekGod, we&apos;re more than just a clothing brand;
+              we&apos;re a movement dedicated to empowering you to
               express your authentic self.
               Just as the Greek gods and goddesses commanded
               their destinies, we want our clothing to inspire you
               to embrace your unique power and style.</p>
-            <p className="text-xl">Every stitch, every design, and every piece we create channels the strength, elegance, and confidence of ancient legends reimagined for the modern world. Whether you're conquering your day, making a statement, or rewriting your story, GreekGod is your armor.
-This isn't just fashion. It's mythology in motion. It's your era. Rule it.</p>
+            <p className="text-xl">Every stitch, every design, and every piece we create channels the strength, elegance, and confidence of ancient legends reimagined for the modern world. Whether you&apos;re conquering your day, making a statement, or rewriting your story, GreekGod is your armor.
+              This isn&apos;t just fashion. It&apos;s mythology in motion. It&apos;s your era. Rule it.</p>
           </div>
         </div>
         <div className="w-full flex flex-col gap-10 md:gap-0 md:flex-row">
           <div className="flex-1 flex flex-col gap-5 md:p-10 justify-center text-start">
-            <p className="text-xl">At GreekGod, we're more than just a clothing brand; we're a movement dedicated to empowering you to express your authentic self. Just as the Greek gods and goddesses commanded their destinies, we want our clothing to inspire you to embrace your unique power and style.</p>
+            <p className="text-xl">At GreekGod, we&apos;re more than just a clothing brand; we&apos;re a movement dedicated to empowering you to express your authentic self. Just as the Greek gods and goddesses commanded their destinies, we want our clothing to inspire you to embrace your unique power and style.</p>
             <p className="text-xl">From bold statements to timeless essentials, our
               designs are created to make you feel confident,
               strong, and undeniably divine.</p>
