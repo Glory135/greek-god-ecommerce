65298adb183bce62886c766fd5f831a58baea110
Glory135 <adeyemiglr@gmail.com>
2025-07-18 10:35:49 +0100

    Update page.tsx

--- src/app/(app)/(frontend)/collections/[collection]/page.tsx
+++ src/app/(app)/(frontend)/collections/[collection]/page.tsx
@@ -1,5 +1,4 @@
 import Collectionproducts from "@/components/collectionsComponents/Collectionproducts"
-import MaxWidthWrapper from "@/components/MaxWidthWrapper"
 // import { getQueryClient, trpc } from "@/trpc/server"
 // import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
 
