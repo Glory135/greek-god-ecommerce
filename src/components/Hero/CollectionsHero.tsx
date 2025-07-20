d9e4ec233cc37bd4e84686cefdb07e8c935e14ae
Glory135 <adeyemiglr@gmail.com>
2025-07-20 04:14:25 +0100

    update

--- src/components/Hero/CollectionsHero.tsx
+++ src/components/Hero/CollectionsHero.tsx
@@ -27,15 +27,15 @@ const CollectionsHero = () => {
       className={`relative w-full bg-center bg-cover bg-no-repeat min-h-[500px] flex items-end py-20 px-5`}>
       <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-black/20 to-transparent transition-opacity duration-300"></div>
 
-    <MaxWidthWrapper>
-      <div className="relative flex w-fit max-w-full flex-col gap-5">
+      <MaxWidthWrapper>
+        <div className="relative flex w-fit max-w-full flex-col gap-5">
           {
-            data?.docs[0]?.title && (
+            data?.docs[0] && data?.docs[0]?.title && (
               <h1 className="text-3xl font-bold drop-shadow-lg text-white animate-in slide-in-from-bottom-4 fade-in duration-700 ease-out">{data?.docs[0]?.title}</h1>
             )
           }
           {
-            data?.docs[0]?.description && (
+            data?.docs[0] && data?.docs[0]?.description && (
               <p className='text-white text-xl max-w-[450px] animate-in slide-in-from-bottom-4 fade-in duration-700 ease-out delay-200'>
                 {data?.docs[0]?.description}
               </p>
